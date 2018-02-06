c893d42f3df935a1b3700b0ca15c41d721432ba1
templarjer <templarjer@sigono.com>
2018-02-06 18:19:54 +0800

    death, camera system finished.

--- PACE Shooter/Assets/Scripts/PlayerController.cs
+++ PACE Shooter/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour {
 
@@ -12,7 +13,18 @@ public class PlayerController : MonoBehaviour {
 	public GameObject Player;
 	public float bulletVelocity = 5f;
 	public GameObject bullet;
-	private int deathcount = 0;
+	public Camera GameCamera;
+	public float CameraShakeDuration;
+	public float CameraShakeStrength;
+	private float OrthoSizeA = 3f;
+	private float OrthoSizeB = 0.02f;
+	private float SmoothZoomt = 5f;
+	private bool DeathZoom = false;
+	private float elapsed = 0.0f;
+	private int health = 1;
+	private bool DeathCheck = false;
+	public BoxCollider2D boxcollider;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,62 +32,90 @@ public class PlayerController : MonoBehaviour {
 	}
 	float speed = 6;
 	// Update is called once per frame
+
 	void OnTriggerEnter2D(Collider2D col)
 	{
+		
+	
 		//	deathcount = deathcount +1
 		if ((col.tag == "EBLTS")) {
-			Debug.Log ("asdasd");
+			
+			if (health != 0) {
+				health -= 1;
+				GameCamera.transform.DOShakePosition (CameraShakeDuration, CameraShakeStrength);
+			}
+				else
+			{	DeathZoom = true;
+				DeathCheck = true;
+			}
 		}
 		}
-	void Update () {
 
-		Vector2 force2D = Vector2.zero;
+	void FixedUpdate () {
 
-		if (Input.GetKey (KeyCode.W)) {
-			//this.transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
-			force2D.y += forceValue;
+		if (DeathZoom) {
+			elapsed += Time.deltaTime / SmoothZoomt;
+			GameCamera.orthographicSize = Mathf.Lerp (OrthoSizeA, OrthoSizeB, elapsed);
+			if (elapsed >= 1.0f) {
+				DeathZoom = false;
+			}
 		}
-		if (Input.GetKey (KeyCode.S)) {
-			//this.transform.position += new Vector3 (0, -speed * Time.deltaTime, 0);
-			force2D.y -= forceValue;
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			force2D.x -= forceValue;
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			force2D.x += forceValue;
-		}
-		if (force2D != Vector2.zero) {
+
+		if (DeathCheck != true) {
+			Vector2 force2D = Vector2.zero;
+
+			if (Input.GetKey (KeyCode.W)) {
+				//this.transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+				force2D.y += forceValue;
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				//this.transform.position += new Vector3 (0, -speed * Time.deltaTime, 0);
+				force2D.y -= forceValue;
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				force2D.x -= forceValue;
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				force2D.x += forceValue;
+			}
+			if (force2D != Vector2.zero) {
+				rigidbody2D.AddForce (force2D);
+				float speed = rigidbody2D.velocity.magnitude;
+				if (speed > MaxSpeed) {
+					speed = MaxSpeed;
+				}
+				rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
+			} else {
+				float speed = rigidbody2D.velocity.magnitude;
+				speed -= decreasingSpeed * Time.deltaTime;
+				if (speed < 0) {
+					speed = 0;
+				}
+				rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
+			}
+
 			rigidbody2D.AddForce (force2D);
-			float speed = rigidbody2D.velocity.magnitude;
-			if (speed > MaxSpeed) {
-				speed = MaxSpeed;
+			if (DeathCheck) {
+				rigidbody2D.mass = 1000f;
+				Debug.Log ("MASS INCREASED");
 			}
-			rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
-		} else {
-			float speed = rigidbody2D.velocity.magnitude;
-			speed -= decreasingSpeed * Time.deltaTime;
-			if (speed < 0) {
-				speed = 0;
+			if (Input.GetButtonDown ("Fire1")) {
+				Vector3 worldMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				Vector2 direction = (Vector2)((worldMousePos - transform.position));
+				direction.Normalize ();
+				// Creates the bullet locally
+				GameObject bullet = (GameObject)Instantiate (
+					                    bulletCandidate,
+					                    transform.position + (Vector3)(direction * 0.5f),
+					                    Quaternion.identity);
+				// Adds velocity to the bullet
+				bullet.GetComponent<Rigidbody2D> ().velocity = direction * bulletVelocity;
 			}
-			rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
 		}
 
-		rigidbody2D.AddForce (force2D);
 
-		if (Input.GetButtonDown ("Fire1")) {
-			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			Vector2 direction = (Vector2)((worldMousePos - transform.position));
-			direction.Normalize ();
-			// Creates the bullet locally
-			GameObject bullet = (GameObject)Instantiate (
-				                    bulletCandidate,
-				                    transform.position + (Vector3)(direction * 0.5f),
-				                    Quaternion.identity);
-			// Adds velocity to the bullet
-			bullet.GetComponent<Rigidbody2D> ().velocity = direction * bulletVelocity;
-		}
+			
+
 	}
-
 	}
 
