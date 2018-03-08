416b214c1fa71a280cef4c8a378a0ba15cef9576
templarjer <templarjer@sigono.com>
2018-03-08 16:32:45 +0800

    Snapshot Completed

--- PACE Shooter/Assets/SnapshotManager.cs
+++ PACE Shooter/Assets/SnapshotManager.cs
@@ -9,14 +9,8 @@ public class SnapshotManager : MonoBehaviour {
 	public AudioMixerSnapshot Stage2S_;
 	public AudioMixerSnapshot Stage3S_;
 	public AudioMixerSnapshot Stage4S_;
-	public AudioMixerSnapshot Stage42S_;
+	public AudioMixerSnapshot Stage6S_;
 	public AudioMixerSnapshot Stage5S_;
-	public BoxCollider2D Stage1C_;
-	public BoxCollider2D Stage2C_;
-	public BoxCollider2D Stage3C_;
-	public BoxCollider2D Stage4C_;
-	public BoxCollider2D Stage42C_;
-	public BoxCollider2D Stage5C_;
 	public float SmoothTimer;
 
 	void Awake ()
@@ -28,27 +22,27 @@ public class SnapshotManager : MonoBehaviour {
 	{
 		if (coll.tag == "SnapshotStage1") {
 			//Debug.Log ("Snapshot1 Activated!");
-			Stage1S_.TransitionTo (SmoothTimer);
+			Stage1S_.TransitionTo (SmoothTimer/2);
 		}
 		if (coll.tag == "SnapshotStage2") {
 			//Debug.Log ("Snapshot2 Activated!");
-			Stage2S_.TransitionTo (SmoothTimer);
+			Stage2S_.TransitionTo (SmoothTimer/2);
 		}
 		if (coll.tag == "SnapshotStage3") {
 			//Debug.Log ("Snapshot3 Activated!");
-			Stage3S_.TransitionTo (SmoothTimer);
+			Stage3S_.TransitionTo (SmoothTimer/2);
 		}
 		if (coll.tag == "SnapshotStage4") {
-			//Debug.Log ("Snapshot4 Activated!");
-			Stage4S_.TransitionTo (SmoothTimer);
+			Debug.Log ("Snapshot4 Activated!");
+			Stage4S_.TransitionTo (SmoothTimer/2);
 		}
-		if (coll.tag == "SnapshotStage42") {
-			//Debug.Log ("Snapshot4 Activated!");
-			Stage42S_.TransitionTo (SmoothTimer);
+		if (coll.tag == "SnapshotStage6") {
+			Debug.Log ("Snapshot4 Activated!");
+			Stage6S_.TransitionTo (3f);
 		}
 		if (coll.tag == "SnapshotStage5") {
-			//Debug.Log ("Snapshot5 Activated!");
-			Stage5S_.TransitionTo (1f);
+			Debug.Log ("Snapshot5 Activated!");
+			Stage5S_.TransitionTo (0);
 		}
 	}
 
