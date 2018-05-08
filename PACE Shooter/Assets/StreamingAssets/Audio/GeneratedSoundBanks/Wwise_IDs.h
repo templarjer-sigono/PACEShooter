eae2f44ed82d41794b8771e4718b0a4a546b79c4
templarjer <templarjer@sigono.com>
2018-05-08 13:56:56 +0800

    disintegrated wwise and then re-integrated.

--- PACE Shooter/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ PACE Shooter/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,65 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ENEMY_BOSS_DEATH = 3388699230U;
+        static const AkUniqueID ENEMY_BOSS_HURT = 3891866179U;
+        static const AkUniqueID ENEMY_DEATH = 1205999388U;
+        static const AkUniqueID ENEMY_SHOOT = 1050776119U;
+        static const AkUniqueID PLAY_BOSSWIND = 1727127563U;
         static const AkUniqueID PLAY_CHANT = 2984543794U;
-        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_CRICKET = 3829433163U;
+        static const AkUniqueID PLAY_GARAGE = 897895173U;
+        static const AkUniqueID PLAY_NOISE = 1932796752U;
         static const AkUniqueID PLAY_RAIN = 2838936948U;
+        static const AkUniqueID PLAY_SCREAM = 2470536401U;
+        static const AkUniqueID PLAYER_HEARTBEAT = 3484321931U;
+        static const AkUniqueID PLAYER_SHOOT = 4004702906U;
     } // namespace EVENTS
-
-    namespace STATES
-    {
-        namespace POSTYPE
-        {
-            static const AkUniqueID GROUP = 2742602365U;
-
-            namespace STATE
-            {
-                static const AkUniqueID EXTERIOR = 2213992659U;
-                static const AkUniqueID INTERIOR = 1132214669U;
-            } // namespace STATE
-        } // namespace POSTYPE
-
-    } // namespace STATES
-
-    namespace SWITCHES
-    {
-        namespace FOOTSTEPS
-        {
-            static const AkUniqueID GROUP = 2385628198U;
-
-            namespace SWITCH
-            {
-                static const AkUniqueID CONCRETE = 841620460U;
-                static const AkUniqueID DIRT = 2195636714U;
-            } // namespace SWITCH
-        } // namespace FOOTSTEPS
-
-        namespace NEW_SWITCH_GROUP
-        {
-            static const AkUniqueID GROUP = 2250165792U;
-
-            namespace SWITCH
-            {
-            } // namespace SWITCH
-        } // namespace NEW_SWITCH_GROUP
-
-    } // namespace SWITCHES
-
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID NEW_GAME_PARAMETER = 3671138082U;
-    } // namespace GAME_PARAMETERS
-
-    namespace TRIGGERS
-    {
-        static const AkUniqueID NEW_TRIGGER = 4163741908U;
-    } // namespace TRIGGERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID AMBIENT = 77978275U;
-        static const AkUniqueID FOOTSTEPS_SOUNDBANK = 1183542248U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
     } // namespace BANKS
 
     namespace BUSSES
@@ -81,7 +41,7 @@ namespace AK
 
     namespace AUX_BUSSES
     {
-        static const AkUniqueID REVERB_SEND = 1310632874U;
+        static const AkUniqueID REVERB_BUS = 2917229290U;
     } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
